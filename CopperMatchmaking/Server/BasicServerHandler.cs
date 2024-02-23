215614e126de6c5bb0669c40d737e31122158d69
AlexRak2 <alejandrorak2@gmail.com>
2024-02-23 17:41:14 -0500

    Added verification for app ownership

--- CopperMatchmaking/Server/BasicServerHandler.cs
+++ CopperMatchmaking/Server/BasicServerHandler.cs
@@ -1,4 +1,8 @@
 using CopperMatchmaking.Data;
+using CopperMatchmaking.Util;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace CopperMatchmaking.Server
 {
@@ -14,7 +18,41 @@ namespace CopperMatchmaking.Server
         /// <returns>True if client is allowed to connect</returns>
         public bool VerifyPlayer(ConnectedClient client)
         {
+            bool isAppOwned = PlayerOwnsApp(client);
+
+            Console.WriteLine("Player Owned App: " + isAppOwned);
+
+            if (!isAppOwned) return false;
+
+
             return true;
         }
+
+        private bool PlayerOwnsApp(ConnectedClient client)
+        {
+            try
+            {
+                var parameters = new Dictionary<string, object>
+                {
+                    { "key", SteamAPIHelper.STEAM_PUBLISHER_WEB_API_KEY },
+                    { "steamid", client.PlayerId.ToString() },
+                    { "appid", SteamAPIHelper.STEAM_APP_ID }
+                };
+
+                string response = SteamAPIHelper.QueryApi("ISteamUser/CheckAppOwnership/v2/", parameters).GetAwaiter().GetResult();
+
+                var jsonObject = JsonDocument.Parse(response).RootElement;
+                var ownsApp = jsonObject.GetProperty("appownership").GetProperty("ownsapp").GetBoolean();
+
+                return ownsApp;
+            }
+            catch (Exception ex)
+            {
+                //Returns false if steam if is not valid, will be better once we authenticate user first
+                Console.WriteLine($"Exception during Steam API verification: {ex.Message}");
+                return false;
+            }
+        }
+
     }
 }
---
+++ CopperMatchmaking/Util/SteamAPIHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopperMatchmaking.Util
+{
+    internal class SteamAPIHelper
+    {
+        public const string STEAM_API_URL = "https://partner.steam-api.com/";
+        public const string STEAM_APP_ID = "2149290";
+        public const string STEAM_PUBLISHER_WEB_API_KEY = "2131582260089A7AC57871BD68275ED2";
+
+        public static async Task<string> QueryApi(string endPoint, Dictionary<string, object> parameters)
+        {
+            string url = ConstructApiUrl(endPoint, parameters);
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return await response.Content.ReadAsStringAsync();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception: {ex.Message}");
+                    return "";
+                }
+
+                return "";
+            }
+        }
+
+        static string ConstructApiUrl(string endPoint, Dictionary<string, object> parameters)
+        {
+            string url = STEAM_API_URL + endPoint + "?" + string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"));
+            return url;
+        }
+    }
+}
